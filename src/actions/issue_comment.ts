8db675012cb742f4d9a5ade137ac94db6f499138
sobolevn <mail@sobolevn.me>
2019-10-08 00:22:22 +0300

    Fixes actions

--- src/actions/issue_comment.ts
+++ src/actions/issue_comment.ts
@@ -1,6 +1,7 @@
 import hasBadWords from '../words'
 
 export function checkIssueComment (context): void {
+  console.log(`log: ${context}`)
   const result = hasBadWords(`${context.payload.comment.body}`)
   console.log(`log: ${result}`)
 }
