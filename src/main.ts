82cb1860d0924e05a14474448538f16e057fb5e6
sobolevn <mail@sobolevn.me>
2019-10-07 23:55:17 +0300

    Fixes actions

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
+// TODO: check commit comments
 async function run (): Promise<void> {
   try {
     // const token = core.getInput('GITHUB_TOKEN')
     // const octokit = new github.GitHub(token)
     const context = github.context
+    const cuss = require('cuss/index.json')
 
     const payload = context.payload.issue || context.payload.pull_request
     if (!payload) {
@@ -13,10 +15,20 @@ async function run (): Promise<void> {
       return
     }
 
-    core.debug(payload.body || 'no body')
-    core.debug(JSON.stringify(context))
-    console.log(payload.body || 'no body')
-    console.log(JSON.stringify(context))
+    const allText = `${payload.title} ${payload.body}`
+    core.debug(`debug: ${allText}`)
+    console.log(`log: ${allText}`)
+
+    const anyBadWords = Object.keys(cuss).some(
+      (word) => allText.includes(word),
+    )
+
+    if (anyBadWords) {
+      core.setFailed('Payload is missing from the action')
+    } else {
+      core.debug('debug: all good')
+      console.log('log: all good')
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
